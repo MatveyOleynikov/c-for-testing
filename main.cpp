7c864f5dfc86ce9985a6fa89c3ee4e0e76d31142
MatveyOleynikov <30356831+MatveyOleynikov@users.noreply.github.com>
2021-10-09 22:05:43 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+ 
+void fast() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+ 
+void solve(){
+    return;
+}
+ 
+void test_beg(ll q){
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    cin >> q;
+}
+ 
+void test_end(){
+    fclose(stdout);
+    freopen("CON", "w", stdout);
+    ifstream fin1("output.txt", ios::binary);
+    ifstream fin2("ex_output.txt", ios::binary);
+    if (fin2 && fin2)
+    {
+        char ch1, ch2;
+        bool result = true;
+        while (fin1.get(ch1) && fin2.get(ch2))
+        {
+            if (ch1 != ch2)
+            {
+                result = false;
+                break;
+            }
+        }
+        if (result && !(fin1.get(ch1) || fin2.get(ch2)) || (ch1 == '\n' || ch2 == '\n'))
+            cout << "OK" << endl;
+        else
+            cout << "WA" << endl;
+    }
+    else
+        cout << "Error opening file!" << endl;
+ 
+}
+ 
+int main() {
+    fast();
+    ll q = 1;
+    //test_beg(q);
+    while (q--){
+        ll t = 1;
+        //cin >> t;
+        while (t--){
+            solve();
+        }
+    }
+    //test_end();
+    return 0;
+}
